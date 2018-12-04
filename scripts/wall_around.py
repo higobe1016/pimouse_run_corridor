6742852f1fe1d9743d4e388fa402f0e04c02b937
Kohei Higo <higobe1016@gmail.com>
2018-12-04 16:13:26 +0000

    wall around

--- scripts/wall_around.py
+++ scripts/wall_around.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 #encoding: utf-8
 
+#motors.py
+#Copyright (c) 2016 Ryuichi Ueda <ryuichiueda@gmail.com>
+#This software is released under the MIT License.
+#http://opensource.org/licenses/mit-license.php
+
 import rospy,copy,math
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallTrace():
+class WallAround():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
@@ -16,43 +21,42 @@ class WallTrace():
     def callback_lightsensors(self,messages):
         self.sensor_values = messages
 
+    def wall_front(self,ls):
+        return ls.left_forward > 50 or ls.right_forward > 50
+
+    def too_right(self,ls):
+        return ls.right_side > 50
+     
+    def too_left(self,ls):
+        return ls.left_side > 50
+
     def run(self):
         rate = rospy.Rate(20)
         data = Twist()
 
-        accel = 0.02
+        data.linear.x = 0.3
+        data.angular.z = 0.0
         while not rospy.is_shutdown():
-            s = self.sensor_values
-            # data.linear.x += accel
-
-            if s.sum_forward >= 50:
-                data.linear.x = 0.0
-            elif data.linear.x <= 0.2:
-                data.linear.x = 0.2
-            elif data.linear.x >= 0.8:
-                data.linear.x = 0.8
-
-            if data.linear.x < 0.2:
-                data.angular.z = 0.0
-            elif s.left_side < 10:
-                data.angular.z = 0.0
+            if self.wall_front(self.sensor_values):
+                data.angular.z = - math.pi
+            elif self.too_right(self.sensor_values):
+                data.angular.z = math.pi
+            elif self.too_left(self.sensor_values):
+                data.angular.z = - math.pi
             else:
-                target = 50
-                #1cm近づくと値がだいたい50増える
-                error = (target - s.left_side)/50.0
-                #1cmあたり3[deg/s]変化をつける
-                data.angular.z = error * 10 * math.pi / 180.0
-
+                e = 1.0 * (50 - self.sensor_values.left_side)
+                data.angular.z = e * math.pi / 180.0
+                
             self.cmd_vel.publish(data)
             rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('wall_trace')
+    rospy.init_node('wall_around')
 
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
 
-    w = WallTrace()
+    w = WallAround()
     w.run()
